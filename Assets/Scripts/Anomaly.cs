c1f0f275f639376e394bd8500068f00f2358974f
Szymon Gościniewicz <Szym2533@gmail.com>
2026-07-21 18:13:24 +0200

    Add working anomalies
    Add two simple kitchen anomalies. Tweak anomalies controller and anomalyy class. Add button to spawn an anomaly instantly.

--- Assets/Scripts/Anomaly.cs
+++ Assets/Scripts/Anomaly.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Anomaly
+public abstract class Anomaly : MonoBehaviour
 {
     public static List<string> Types = new List<string> {"Object moved", "Object disappeared", "Extra object", "Intruder"};
-    public List<string> Type { get; protected set; }
+
+    [SerializeField] public string name;
+    public List<string> Type { get; protected set; } = new List<string>();
     public string Room { get; protected set; }
     public abstract void Appear();
     public abstract void Disappear();
